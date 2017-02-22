4a8fee6b5588b30609542b20fd451e8a77944738
Jacob Devera <jacob.devera12@gmail.com>
2017-02-21 22:34:41 -0800

    Fire toast every time alarm activates

---
+++ app/src/main/java/edu/washington/gjdevera/awty/AlarmReceiver.java
@@ -0,0 +1,21 @@
+package edu.washington.gjdevera.awty;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    public AlarmReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String phone = intent.getStringExtra("phone");
+        String message = intent.getStringExtra("message");
+        Toast.makeText(context, String.format(context.getResources().getString(R.string.sent), phone, message),
+                Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/edu/washington/gjdevera/awty/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/awty/MainActivity.java
@@ -1,13 +1,24 @@
 package edu.washington.gjdevera.awty;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean started = false;
+    private long intentId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,54 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final TextInputEditText message = (TextInputEditText) findViewById(R.id.message);
+        final TextInputEditText phone = (TextInputEditText) findViewById(R.id.phone);
+        final TextInputEditText interval = (TextInputEditText) findViewById(R.id.interval);
+
         final Button btn =  (Button) findViewById(R.id.start);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!started) {
+                    if (TextUtils.isEmpty(message.getText().toString())) {
+                        Toast.makeText(getApplication(),
+                                "Please enter a message", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    boolean phoneValid = android.util.Patterns.PHONE.matcher(phone.getText()).matches();
+                    if (!phoneValid) {
+                        Toast.makeText(getApplication(),
+                                "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    String intervalString = interval.getText().toString();
+                    boolean intervalValid = TextUtils.isDigitsOnly(interval.getText())
+                            && !intervalString.equals("") && Integer.parseInt(intervalString) > 0;
+                    if (!intervalValid) {
+                        Toast.makeText(getApplication(),
+                                "Please enter an interval greater than 0", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    // passed validation checks
+                    btn.setText(getResources().getString(R.string.stop));
+                    started = true;
 
+                    intentId = System.currentTimeMillis();
+                    Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+                    intent.putExtra("phone", phone.getText().toString());
+                    intent.putExtra("message", message.getText().toString());
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 1001, intent, 0);
+                    int intervalTimer = Integer.parseInt(intervalString) * 1000 * 60;
+                    AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), intervalTimer, pendingIntent);
+                } else {
+                    btn.setText(getResources().getString(R.string.start));
+                    started = false;
+                    Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 1001, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    manager.cancel(pendingIntent);
+                }
             }
         });
     }
