c1d04f806f805ccf7c692b91f3d9ace1e1a42e34
Jacob Devera <jacob.devera12@gmail.com>
2017-02-21 21:32:46 -0800

    Added EditText fields

--- app/src/main/java/edu/washington/gjdevera/awty/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/awty/MainActivity.java
@@ -2,12 +2,27 @@ package edu.washington.gjdevera.awty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button btn =  (Button) findViewById(R.id.start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
