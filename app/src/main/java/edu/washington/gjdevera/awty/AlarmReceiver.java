d191a92611ac8d122281bc411e4b7968f984906a
Jacob Devera <jacob.devera12@gmail.com>
2017-02-26 15:18:14 -0800

    Use intent to send SMS

--- app/src/main/java/edu/washington/gjdevera/awty/AlarmReceiver.java
+++ app/src/main/java/edu/washington/gjdevera/awty/AlarmReceiver.java
@@ -3,6 +3,8 @@ package edu.washington.gjdevera.awty;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -17,5 +19,21 @@ public class AlarmReceiver extends BroadcastReceiver {
         String message = intent.getStringExtra("message");
         Toast.makeText(context, String.format(context.getResources().getString(R.string.sent), phone, message),
                 Toast.LENGTH_LONG).show();
+
+        Log.i("Send SMS", "");
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+
+        smsIntent.setData(Uri.parse("smsto:"));
+        smsIntent.setType("vnd.android-dir/mms-sms");
+        smsIntent.putExtra("address", phone);
+        smsIntent.putExtra("sms_body", message);
+
+        try {
+            context.startActivity(smsIntent);
+            Log.i("Finished sending SMS...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(context,
+                    "SMS sending failed, please try again later.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
