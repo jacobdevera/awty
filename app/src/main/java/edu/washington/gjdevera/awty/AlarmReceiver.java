6c0d456af26807e9fdf68977c94f5b5d6e48b437
Jacob Devera <jacob.devera12@gmail.com>
2017-02-26 15:27:57 -0800

    Readded permission

--- app/src/main/java/edu/washington/gjdevera/awty/AlarmReceiver.java
+++ app/src/main/java/edu/washington/gjdevera/awty/AlarmReceiver.java
@@ -3,8 +3,7 @@ package edu.washington.gjdevera.awty;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
+import android.telephony.SmsManager;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -20,13 +19,6 @@ public class AlarmReceiver extends BroadcastReceiver {
         Toast.makeText(context, String.format(context.getResources().getString(R.string.sent), phone, message),
                 Toast.LENGTH_LONG).show();
 
-        Log.i("Send SMS", "");
-        try {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts(message, phone, null)));
-            Log.i("Finished sending SMS...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(context,
-                    "SMS sending failed, please try again later.", Toast.LENGTH_SHORT).show();
-        }
+        SmsManager.getDefault().sendTextMessage(phone, null, message, null, null);
     }
 }
